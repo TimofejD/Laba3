2959084cf76003500997a25df53ae47ab66078c3
TimofejD <timofejdd@gmail.com>
2022-05-12 20:18:06 +0500

    Lesson 17

--- Laba 3.cpp
+++ Laba 3.cpp
@@ -8,19 +8,13 @@
 #include <Magick++/Image.h>
 #define WINDOW_WIDTH 1024
 #define WINDOW_HEIGHT 768
-
-GLuint VBO;
-GLuint IBO;
-GLuint gWorldLocation;
-GLuint gSampler;
+GLint success;
 
 static float scale = 0.0f;
 using namespace std;
 
-//const char* pVS = "#version 330\n layout (location = 0) in vec3 Position; layout (location = 1) in vec2 TexCoord; uniform mat4 gWorld; out vec2 TexCoord0; void main() {gl_Position = gWorld * vec4(Position, 1.0); TexCoord0 = TexCoord; }";
-//const char* pFS = "#version 330\n in vec2 TexCoord0; out vec4 FragColor; uniform sampler2D gSampler; void main() {FragColor = texture2D(gSampler, TexCoord0.xy);;}";
 static const char* pVS = "                                                      \n\
-    #version 330                                                                   \n\
+    #version 410                                                                   \n\
     layout (location = 0) in vec3 pos;                                             \n\
     layout (location = 1) in vec2 tex;                                             \n\
     uniform mat4 gWorld;                                                           \n\
@@ -32,13 +26,21 @@ static const char* pVS = "                                                      
     }";
 
 static const char* pFS = "                                                         \n\
-    #version 330                                                                    \n\
+    #version 410                                                                    \n\
     in vec2 tex0;                                                                   \n\
+    struct DirectionalLight                                                         \n\
+    {                                                                               \n\
+        vec3 Color;                                                                 \n\
+        float AmbientIntensity;                                                     \n\
+    };                                                                              \n\
     uniform sampler2D gSampler;                                                     \n\
+    uniform DirectionalLight gDirectionalLight;                                     \n\
     out vec4 fragcolor;                                                             \n\
     void main()                                                                     \n\
     {                                                                               \n\
-        fragcolor = texture2D(gSampler, tex0.xy);                                   \n\
+        fragcolor = texture2D(gSampler, tex0.xy)*                                   \n\
+        vec4(gDirectionalLight.Color, 1.0f) *                                       \n\
+        gDirectionalLight.AmbientIntensity;											\n\
     }";
 
 struct vertex {
@@ -50,55 +52,6 @@ struct vertex {
 		snd = inp2;
 	}
 };
-
-static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
-{
-	GLuint ShaderObj = glCreateShader(ShaderType);
-
-	if (ShaderObj == 0) {
-		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-		exit(0);
-	}
-
-	const GLchar* p[1];
-	p[0] = pShaderText;
-	
-	glShaderSource(ShaderObj, 1, p, NULL);
-	glCompileShader(ShaderObj);
-
-	GLint success;
-	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		GLchar InfoLog[1024];
-		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-		exit(1);
-	}
-
-	glAttachShader(ShaderProgram, ShaderObj);
-}
-
-static void CompileShaders(GLuint ShaderProgram)
-{
-
-	GLint success;
-
-	glLinkProgram(ShaderProgram);
-
-	glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &success);
-	if (success == 0) {
-		GLchar ErrorLog[1024];
-		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-		exit(1);
-	}
-
-	glValidateProgram(ShaderProgram);
-
-	glUseProgram(ShaderProgram);
-	gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
-	assert(gWorldLocation != 0xFFFFFFFF);
-}
 
 glm::mat4x4 RotMat(float RotateX, float RotateY, float RotateZ)
 {
@@ -307,108 +260,335 @@ private:
 	Magick::Blob m_blob;
 };
 Texture* pTexture = NULL;
-void RenderSceneCB()
+class Technique
 {
-	glClear(GL_COLOR_BUFFER_BIT); //очистка буфера кадра
-	
-	scale += 0.015f;
-	Pipeline p;
-	//p.Scale(sinf(scale * 0.1f), sinf(scale * 0.1f), sinf(scale * 0.1f));
-	//p.WorldPos(0.0f, 0.0f, sinf(scale));
-	p.Rotate(sinf(scale) * 2, 0.0f, sinf(scale));
-	p.SetPerspectiveProj(30.0f, GLUT_WINDOW_WIDTH, GLUT_WINDOW_HEIGHT, 1.0f, 100.0f);
+public:
 
-	glm::vec3 CameraPos(0.0f, 0.0f, -3.0f);
-	glm::vec3 CameraTarget(0.0f, 0.0f, 2.0f);
-	glm::vec3 CameraUp(0.0f, 1.0f, 0.0f);
-	p.SetCamera(CameraPos, CameraTarget, CameraUp);
-	
-	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.GetTrans());
+	Technique() { m_shaderProg = 0; };
 
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	~Technique()
+	{
+		for (ShaderObjList::iterator it = m_shaderObjList.begin(); it != m_shaderObjList.end(); it++) {
+			glDeleteShader(*it);
+		}
+		if (m_shaderProg != 0) {
+			glDeleteProgram(m_shaderProg);
+			m_shaderProg = 0;
+		}
+	}
+	virtual bool Init()
+	{
+		m_shaderProg = glCreateProgram();
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), 0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (const GLvoid*)12);
+		if (m_shaderProg == 0) {
+			GLchar InfoLog[1024];
+			cerr << "Error creating shader program: " << InfoLog << endl;
+			return false;
+		}
+		return true;
+	}
+	void Enable() { glUseProgram(m_shaderProg); };
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	pTexture->Bind(GL_TEXTURE0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+protected:
 
-	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-	glDisableVertexAttribArray(0);
-	glDisableVertexAttribArray(1);
+	bool AddShader(GLenum ShaderType, const char* pShaderText)
+	{
+		GLuint ShaderObj = glCreateShader(ShaderType);
 
-	glutSwapBuffers(); // меняем фоновый буфер и буфер кадра местами
-	glutIdleFunc(RenderSceneCB);
+		if (ShaderObj == 0) {
+			fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+			exit(0);
+		}
 
-}
-static void CreateVertexBuffer()
-{
-	vertex Vertices[4] = {
-		vertex(glm::vec3(-0.2, -0.2, 0),glm::vec2(0,0)),
-	   vertex(glm::vec3(0.3, -0.2, 0.5),glm::vec2(0.5,0)),
-	   vertex(glm::vec3(0.3, -0.2, -0.5),glm::vec2(1,0)),
-	   vertex(glm::vec3(0, 0.4, 0),glm::vec2(0.5,1)),
+		const GLchar* p[1];
+		p[0] = pShaderText;
+
+		glShaderSource(ShaderObj, 1, p, NULL);
+		glCompileShader(ShaderObj);
+
+		
+		glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			GLchar InfoLog[1024];
+			glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+			fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+			exit(1);
+		}
+
+		glAttachShader(m_shaderProg, ShaderObj);
+		return true;
+	}
+
+
+	bool Finalize()
+	{
+		GLint success;
+
+		glLinkProgram(m_shaderProg);
+
+		glGetProgramiv(m_shaderProg, GL_LINK_STATUS, &success);
+		if (success == 0) {
+			GLchar ErrorLog[1024];
+			glGetProgramInfoLog(m_shaderProg, sizeof(ErrorLog), NULL, ErrorLog);
+			fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+			return false;
+		}
+		for (ShaderObjList::iterator it = m_shaderObjList.begin(); it != m_shaderObjList.end(); it++) {
+			glDeleteShader(*it);
+		}
+		m_shaderObjList.clear();
+
+		return true;
+	}
+
+	GLint GetUniformLocation(const char* pUniformName)
+	{
+		GLint Location = glGetUniformLocation(m_shaderProg, pUniformName);
+
+		if (Location == 0xFFFFFFFF) {
+			cerr << "Warning! Unable to get the location of uniform " << pUniformName << endl;
+		}
+		return Location;
 	};
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-}
 
-static void CreateIndexBuffer()
+private:
+
+	GLuint m_shaderProg;
+	typedef list<GLuint> ShaderObjList;
+	ShaderObjList m_shaderObjList;
+};
+
+struct DirectionLight
 {
-	unsigned int Indices[] = { 0, 3, 1,
+	glm::vec3 Color;
+	float AmbientIntensity;
+};
+class LightingTechnique : public Technique {
+public:
+	LightingTechnique() {};
+
+	virtual bool Init() {
+		if (!Technique::Init()) return false;
+		if (!AddShader(GL_VERTEX_SHADER, pVS)) return false;
+		if (!AddShader(GL_FRAGMENT_SHADER, pFS)) return false;
+		if (!Finalize())  return false;
+
+		gWorldLocation = GetUniformLocation("gWorld");
+		samplerLocation = GetUniformLocation("gSampler");
+		dirLightColorLocation = GetUniformLocation("gDirectionalLight.Color");
+		dirLightAmbientIntensityLocation = GetUniformLocation("gDirectionalLight.AmbientIntensity");
+
+		if (dirLightAmbientIntensityLocation == 0xFFFFFFFF || gWorldLocation == 0xFFFFFFFF || samplerLocation == 0xFFFFFFFF || dirLightColorLocation == 0xFFFFFFFF)  return false;
+
+		return true;
+	};
+
+	void SetgWorld(const glm::mat4* gWorld) {
+		glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)gWorld);
+	};
+
+	void SetTextureUnit(unsigned int unit) {
+		glUniform1i(samplerLocation, unit);
+	};
+
+	void SetDirectionalLight(const DirectionLight& Light) {
+		glUniform3f(dirLightColorLocation, Light.Color.x, Light.Color.y, Light.Color.z);
+		glUniform1f(dirLightAmbientIntensityLocation, Light.AmbientIntensity);
+	};
+private:
+	GLuint gWorldLocation;
+	GLuint samplerLocation;
+	GLuint dirLightColorLocation;
+	GLuint dirLightAmbientIntensityLocation;
+};
+
+class ICallbacks
+{
+public:
+	virtual void KeyboardCB(unsigned char Key, int x, int y) = 0;
+	virtual void RenderSceneCB() = 0;
+	virtual void IdleCB() = 0;
+};
+
+void GLUTBackendInit(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	Magick::InitializeMagick(*argv);
+};
+
+bool GLUTBackendCreateWindow(unsigned int Width, unsigned int Height, const char* name) {
+	glutInitWindowSize(Width, Height);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(name);
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		cerr << "Error: " << glewGetErrorString(res) << endl;
+		return false;
+	}
+	return true;
+};
+ICallbacks* ICall = NULL;
+void RenderScene() {
+	ICall->RenderSceneCB();
+}
+void Idle() {
+	ICall->IdleCB();
+}
+void Keyboard(unsigned char Key, int x, int y) {
+	ICall->KeyboardCB(Key, x, y);
+}
+void CB() {
+	glutDisplayFunc(RenderScene);
+	glutIdleFunc(Idle);
+	glutKeyboardFunc(Keyboard);
+}
+void GLUTBackendRun(ICallbacks* p) {
+	if (!p) {
+		fprintf(stderr, "%s : callbacks not specified!\n", __FUNCTION__);
+		return;
+	}
+	ICall = p;
+	CB();
+	glutMainLoop();
+};
+class Main : public ICallbacks
+{
+private:
+	GLuint VBO;
+	GLuint IBO;
+	LightingTechnique* light;
+
+	Texture* texture;
+	DirectionLight dirLight;
+	void GenBuff() {
+		vertex Vertices[4] = {
+		vertex(glm::vec3(-0.2, -0.2, 0),glm::vec2(0,0)),
+		vertex(glm::vec3(0.3, -0.2, 0.5),glm::vec2(0.5,0)),
+		vertex(glm::vec3(0.3, -0.2, -0.5),glm::vec2(1,0)),
+		vertex(glm::vec3(0, 0.4, 0),glm::vec2(0.5,1)),
+		};
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+		unsigned int Indices[] = { 0, 3, 1,
 							   1, 3, 2,
 							   2, 3, 0,
 							   0, 2, 1 };
 
-	glGenBuffers(1, &IBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
-}
+		glGenBuffers(1, &IBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+	}
+public:
+	Main()
+	{
+		pTexture = NULL;
+		light = NULL;
+		dirLight.Color = glm::vec3(1.0f, 1.0f, 1.0f);
+		dirLight.AmbientIntensity = 0.5f;
+	}
+	~Main() {
+		delete light;
+		delete pTexture;
+	};
+	bool Init()
+	{
+		GenBuff();
+		light = new LightingTechnique();
+		if (!light->Init())
+		{
+			return false;
+		}
+		light->Enable();
+		light->SetTextureUnit(0);
 
+		pTexture = new Texture(GL_TEXTURE_2D, "test.png");
+
+		if (!pTexture->Load()) {
+			return false;
+		}
+
+		return true;
+	}
+	void Run()
+	{
+		GLUTBackendRun(this);
+	}
+	virtual void RenderSceneCB()
+	{
+		glClear(GL_COLOR_BUFFER_BIT); //очистка буфера кадра
+
+		scale += 0.015f;
+		Pipeline p;
+		//p.Scale(sinf(scale * 0.1f), sinf(scale * 0.1f), sinf(scale * 0.1f));
+		//p.WorldPos(0.0f, 0.0f, sinf(scale));
+		p.Rotate(sinf(scale) * 2, 0.0f, sinf(scale));
+		p.SetPerspectiveProj(30.0f, GLUT_WINDOW_WIDTH, GLUT_WINDOW_HEIGHT, 1.0f, 100.0f);
+
+		glm::vec3 CameraPos(0.0f, 0.0f, -3.0f);
+		glm::vec3 CameraTarget(0.0f, 0.0f, 2.0f);
+		glm::vec3 CameraUp(0.0f, 1.0f, 0.0f);
+		p.SetCamera(CameraPos, CameraTarget, CameraUp);
+
+		//glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.GetTrans());
+		light->SetgWorld(p.GetTrans());
+		light->SetDirectionalLight(dirLight);
+
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), 0);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (const GLvoid*)12);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		pTexture->Bind(GL_TEXTURE0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+
+		glutSwapBuffers(); // меняем фоновый буфер и буфер кадра местами
+	}
+	virtual void IdleCB()
+	{
+		RenderSceneCB();
+	}
+	virtual void KeyboardCB(unsigned char Key, int x, int y)
+	{
+		switch (Key) {
+		case 'q':
+			glutLeaveMainLoop();
+			break;
+
+		case 'a':
+			dirLight.AmbientIntensity += 0.05f;
+			break;
+
+		case 's':
+			dirLight.AmbientIntensity -= 0.05f;
+			break;
+		}
+	}
+
+};
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv); // инициализация окна
+	GLUTBackendInit(argc, argv);
 
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //установка режима отображения
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT); //задаем размер, позицию окна, даем название
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Laba 3");
-
-	GLenum res = glewInit();
-	if (res != GLEW_OK)
-	{
-		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
-		return 1;
-	}
-	Magick::InitializeMagick(*argv);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	CreateVertexBuffer();
-	CreateIndexBuffer();
-
-	GLuint ShaderProgram = glCreateProgram();
-	AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
-	AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
-
-	CompileShaders(ShaderProgram);
-	glFrontFace(GL_CW);
-	glCullFace(GL_BACK);
-	glEnable(GL_CULL_FACE); 
-	
-	glUniform1i(gSampler, 0);
-	
-	pTexture = new Texture(GL_TEXTURE_2D, "test.png");
-
-	if (!pTexture->Load()) {
+	if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LABA 3")) {
 		return 1;
 	}
 
+	Main* pApp = new Main();
 
-	glutDisplayFunc(RenderSceneCB); //работа в самой оконной системе
-	glutMainLoop(); //зацикливаем и вызываем функцию отображения окна на экран
+	if (!pApp->Init()) return 1;
+
+	pApp->Run();
+
+	delete pApp;
 
 	return 0;
 }
